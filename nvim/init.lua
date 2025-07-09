d73501b184a1ad3f5e845ab00f8c3f8ebe02cbdc
Dan D'Avella <drdavella@gmail.com>
2025-07-09 11:13:20 -0400

    Add auto-formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -255,6 +255,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
+-- Autoformat on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("LspFormatting", {}),
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
+
 -- Autocompletion setup
 local cmp = require("cmp")
 local luasnip = require("luasnip")
